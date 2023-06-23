4b137ab073ea78291392d4deac72a21407128155
silas <silas.oficial.prof@gmail.com>
2023-06-23 00:11:35 -0300

    projeto finalizado

--- adm/delete_produto/index.mjs
+++
@@ -1,8 +0,0 @@
-var inputs = document.getElementsByClassName("input-transform");
-
-for (var i = 0; i < inputs.length; i++) {
-    inputs[i].addEventListener("input", function() {
-        var texto = this.value;
-        this.value = texto.toUpperCase();
-    });
-}
